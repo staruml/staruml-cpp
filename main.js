25c144d7b19f4335e2e3fb9039080ba0dff3f6f5
sung min lee <toori67@gmail.com>
2014-09-29 20:52:32 +0900

    add logging versions, and last sync time.

--- main.js
+++ main.js
@@ -99,4 +99,22 @@ define(function (require, exports, module) {
 	menuItem.addMenuItem(CMD_CPP_REVERSE);
 	menuItem.addMenuDivider();
 	menuItem.addMenuItem(CMD_CPP_CONFIGURE);
+    
+    // for debug 
+    
+    var getCurrentTime = function () {
+        var currentdate = new Date(); 
+        var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+        return datetime;
+    }    
+    console.log("================================================");
+    console.log("Java Code Generator Plugin.");
+    console.log("Version time - " + CppCodeGenerator.getVersion() );
+    console.log(getCurrentTime());
+    console.log("================================================");
 });
